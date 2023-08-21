b4fda94caddd9f821acf5318530f17fdafc3bfef
Roy Contreras Benitez <roy7474@gmail.com>
2023-08-21 18:45:37 -0400

    the last part of the program has been completed. the person who has the most commits is printed at the end of the program

--- chap10_ex1.py
+++ chap10_ex1.py
@@ -19,6 +19,7 @@ zqian@umich.edu 195
 #open program directly for easier testing
 fhand = open('mbox-short.txt')
 emails = {}
+lst = list()
 ''''
 fhand = input('Enter the name of the file that you would like to open. Please include the file format(eg.: file_name.format): ')
 try:
@@ -32,7 +33,7 @@ except FileNotFoundError:
 # Count the number of messages from each person using a dictionary.
 for line in fhand:
     words = line.split()
-    if len(words) < 3 or words[0] != 'From':
+    if  len(words) < 3 or words[0] != 'From':
         continue
     else:
         if words[1] not in emails:
@@ -45,9 +46,20 @@ for line in fhand:
 
 # After all the data has been read, print the person with the most commits by 
 # creating a list of (count, email) tuples from the dictionary. 
-
+'''
 max_key_value = max(emails, key = emails.get)
 max_value = emails[max_key_value]
-print(max_value, 'by', max_key_value)
+print(max_value, 'by', max_key_value) 
 
+for key, value in list(emails.items()): 
+'''
+# Then sort the list in reverse order and print out the person who has the most commits
+
+for key, value in list(emails.items()):
+    lst.append((value, key))
+
+lst.sort(reverse=True)
+
+for key, value in lst[:1]:
+    print(value, key)
 
